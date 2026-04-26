2e9ce327f6bb0e41311ac1bd97507fd8c38dab27
Kartikesh Gaonkar <kartikeshgaonkar2509@gmail.com>
2026-04-26 22:27:05 +0530

    add landing redirect component

---
+++ frontend/src/pages/LandingRedirect.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+
+const LandingRedirect = () => {
+  useEffect(() => {
+    window.location.href = 'https://pretty-diplodocus-240172.framer.app/';
+  }, []);
+
+  return null;
+};
+
+export default LandingRedirect;
